7e6dc3be32b265be1fe1604a6b1efc529c0bd7a6
Ankush <ankushvermabharat@gmail.com>
2024-04-01 22:13:01 +0530

    updat

--- index.py
+++ index.py
@@ -1,1 +1,53 @@
-from wsgi import app
+from flask import Flask, request, jsonify
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pickle
+
+app=Flask(__name__)
+
+with open('model_pickle','rb') as f:
+   mp= pickle.load(f)
+
+
+@app.route('/', methods=['POST'])
+def postit():
+     # Extract data from the POST request
+    data = request.get_json()
+
+    # Extract specific fields from the JSON data
+    area = data.get('area')
+    br = data.get('br')
+    bt = data.get('bt')
+    n = data.get('n')
+    age = data.get('age')
+
+    # Process the data or perform any necessary operations
+    # For demonstration, let's just return the extracted data in a response
+    try:
+        price=mp.predict([[area,br,bt
+                       ,n,age]])[0]
+    except:
+        print("error in prediction")
+        
+    response_data = {
+            "price":price
+        }
+    
+    
+
+    # Return the extracted data in a JSON response
+    return jsonify(response_data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+# {
+#    "area":265456,
+#    "br":2,
+#    "bt":1,
+#    "n":0,
+#    "age":1995
+# }
